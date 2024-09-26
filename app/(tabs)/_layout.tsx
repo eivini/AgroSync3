dff62290607199689d05421cf798dc7e483f2ec1
eivini <marcusvkz@hotmail.com>
2024-09-26 15:14:40 -0300

    Primeiro commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,38 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+        <Tabs.Screen
+          name="cadastro"
+          options={{
+            title: 'Cadastro',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+          }}
+        />
+
+        <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
+            title: 'Home',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
-      />
-      <Tabs.Screen
-        name="explore"
+        />
+        <Tabs.Screen
+        name="settings"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
+            title: 'Settings',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
         }}
-      />
+        />
+        <Tabs.Screen
+        name="saude"
+        options={{
+            title: 'Saude',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="google" color={color} />,
+        }}
+        />
     </Tabs>
   );
 }
--- app/(tabs)/cadastro.tsx
+++ app/(tabs)/cadastro.tsx
@@ -1,9 +1,9 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function Tab() {
+export default function Cadastro() {
   return (
     <View style={styles.container}>
-      <Text>Tab [Home|Settings]</Text>
+      <Text>Teste</Text>
     </View>
   );
 }
